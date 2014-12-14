c8f62c94d31ff4231526e3a718276db870dc7e7f
Munk801 <munk801@gmail.com>
2014-12-14 13:33:03 -0800

    Initial Commit

---
+++ QuizzerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizzerDemo
+//
+//  Created by Stephen Lu on 12/14/14.
+//  Copyright (c) 2014 LineBreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
